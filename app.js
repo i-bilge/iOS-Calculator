39110b3e2107f2a3930fb9a8adfde5d6ead70079
i-bilge <iismailbilge@gmail.com>
2022-06-11 21:01:52 +0200

    operations and 2 output windows are stufied.

--- app.js
+++ app.js
@@ -3,132 +3,259 @@ let output = document.getElementById("output");
 let buttonArea = document.getElementById("buttonArea");
 
 let answer;
-let operation ="";
+let operation = "";
 let firstNumber;
 let secondNumber;
 
 //here we are defining the calculation
 function calculateIt(Num1, Num2, Op, a, b, c) {
-    if (Op == "+") {
-        b += a + c;
-        a = Num1 + Num2;
-      }
-    if (operation == "-") {
-        b += a + c;
-        a = Num1 -  Num2;
-      }
-    if (operation == "*") {
-        b += a + c;
-        a = Num1 * Num2;
-      }
-    if (operation == "/") {
-        b += a + c;
-        a = Num1 / Num2;
-      }
+  if (Op == "+") {
+    b += a + c;
+    a = Num1 + Num2;
+  }
+  if (operation == "-") {
+    b += a + c;
+    a = Num1 - Num2;
+  }
+  if (operation == "*") {
+    b += a + c;
+    a = Num1 * Num2;
+  }
+  if (operation == "/") {
+    b += a + c;
+    a = Num1 / Num2;
+  }
 }
 
 //NOW WE RE STARTING THE EVENTLISTENER
 //if there is no input just take number
 buttonArea.addEventListener("click", (e) => {
-    let x = e.target.textContent;
-    if (!output.textContent) {
-        if (e.target.className == "butons numbers") {
-            output.textContent = e.target.textContent;
-            secondNumber = Number(output.textContent);
+  let x = e.target.textContent;
+  if (!output.textContent) {
+    if (e.target.className == "butons numbers") {
+      output.textContent = e.target.textContent;
+      secondNumber = Number(output.textContent);
 
-            console.log("When Tehere is no input=> Num1: ", firstNumber, "Num2: ",secondNumber, "OP: ",operation, "click: ",e.target.textContent, "a: ",output.textContent, "b: ",calculation.textContent);
-        }
+      console.log(
+        "When Tehere is no input=> Num1: ",
+        firstNumber,
+        "Num2: ",
+        secondNumber,
+        "OP: ",
+        operation,
+        "click: ",
+        e.target.textContent,
+        "a: ",
+        output.textContent,
+        "b: ",
+        calculation.textContent
+      );
     }
-//if there is input:...
-    else {
-        //when user click on numbers
-        if (e.target.className == "butons numbers") {
-            output.textContent += e.target.textContent;
-            secondNumber = Number(output.textContent);
+  }
+  //if there is input:...
+  else {
+    //when user click on numbers
+    if (e.target.className == "butons numbers") {
+      output.textContent += e.target.textContent;
+      secondNumber = Number(output.textContent);
 
-            console.log("Click on num=> Num1: ", firstNumber, "Num2: ",secondNumber, "OP: ",operation, "click: ",e.target.textContent, "a: ",output.textContent, "b: ",calculation.textContent);
-        }
-        //when user click on operations
-        else if(e.target.className == "butons operations") {
-            if (operation != "") {
-                // calculateIt(firstNumber, secondNumber, operation, output.textContent, calculation.textContent, e.target.textContent);
-                if (operation == "+") {
-                    calculation.textContent = firstNumber + secondNumber;
-                    firstNumber = Number(e.target.textContent);
-                  }
-                if (operation == "-") {
-                    calculation.textContent = firstNumber - secondNumber;
-                    output.textContent = e.target.textContent;
-                  }
-                if (operation == "*") {
-                    calculation.textContent = firstNumber * secondNumber;
-                    output.textContent = e.target.textContent;
-                  }
-                if (operation == "/") {
-                    calculation.textContent = firstNumber / secondNumber;
-                    output.textContent = e.target.textContent;
-                  }
-                  console.log("Click nonFirst operation=> Num1: ", firstNumber, "Num2: ",secondNumber, "OP: ",operation, "click: ",e.target.textContent, "a: ",output.textContent, "b: ",calculation.textContent);
-            }
-            else {
-                calculation.textContent += output.textContent + e.target.textContent;
-                firstNumber = secondNumber;
-
-                console.log("Click to First operation=> Num1: ", firstNumber, "Num2: ",secondNumber, "OP: ",operation, "click: ",e.target.textContent, "a: ",output.textContent, "b: ",calculation.textContent);
-            }
-            
-            operation = e.target.textContent;
-            output.textContent = "";    
-            
-            console.log("After clicking operation=> Num1: ", firstNumber, "Num2: ",secondNumber, "OP: ",operation, "click: ",e.target.textContent, "a: ",output.textContent, "b: ",calculation.textContent);
-        }
-        //when user click on equal button
-        else if(e.target.className == "butons equal") {   
-            // calculateIt(firstNumber, secondNumber, operation, output.textContent, calculation.textContent, e.target.textContent);
-            if (operation == "+") {
-                calculation.textContent = "";
-                output.textContent = firstNumber + secondNumber;
-              }
-            if (operation == "-") {
-                calculation.textContent += output.textContent + e.target.textContent;
-                output.textContent = firstNumber -  secondNumber;
-              }
-            if (operation == "*") {
-                calculation.textContent += output.textContent + e.target.textContent;
-                output.textContent = firstNumber * secondNumber;
-              }
-            if (operation == "/") {
-                calculation.textContent += output.textContent + e.target.textContent;
-                output.textContent = firstNumber / secondNumber;
-              }
-
-              console.log("Click on equal => Num1: ", firstNumber, "Num2: ",secondNumber, "OP: ",operation, "click: ",e.target.textContent, "a: ",output.textContent, "b: ",calculation.textContent);
-        }
-        //when user click on additional operators
-        else if(e.target.className == "butons additionalOperations") {
-            if (e.target.textContent == "AC") {
-                output.textContent = "";
-                calculation.textContent = "";
-
-                console.log("Click on AC=> Num1: ", firstNumber, "Num2: ",secondNumber, "OP: ",operation, "click: ",e.target.textContent, "a: ",output.textContent, "b: ",calculation.textContent);
-            }
-            else if(e.target.textContent == "+-") {
-                
-                console.log("Click on +- => Num1: ", firstNumber, "Num2: ",secondNumber, "OP: ",operation, "click: ",e.target.textContent, "a: ",output.textContent, "b: ",calculation.textContent);
-            }
-            else if (e.target.textContent == "%") {
-                calculation.textContent += output.textContent + e.target.textContent;
-                output.textContent = Number(output.textContent)/100;
-
-                console.log("Click on % => Num1: ", firstNumber, "Num2: ",secondNumber, "OP: ",operation, "click: ",e.target.textContent, "a: ",output.textContent, "b: ",calculation.textContent);
-            }
-
-            console.log("After additionalOperations=> Num1: ", firstNumber, "Num2: ",secondNumber, "OP: ",operation, "click: ",e.target.textContent, "a: ",output.textContent, "b: ",calculation.textContent);
-        }
+      console.log(
+        "Click on num=> Num1: ",
+        firstNumber,
+        "Num2: ",
+        secondNumber,
+        "OP: ",
+        operation,
+        "click: ",
+        e.target.textContent,
+        "a: ",
+        output.textContent,
+        "b: ",
+        calculation.textContent
+      );
     }
+    //when user click on operations
+    else if (e.target.className == "butons operations") {
+      if (operation != "") {
+        // calculateIt(firstNumber, secondNumber, operation, output.textContent, calculation.textContent, e.target.textContent);
+        if (operation == "+") {
+          calculation.textContent = firstNumber + secondNumber;
+          firstNumber = Number(e.target.textContent);
+        }
+        if (operation == "-") {
+          calculation.textContent = firstNumber - secondNumber;
+          output.textContent = e.target.textContent;
+        }
+        if (operation == "*") {
+          calculation.textContent = firstNumber * secondNumber;
+          output.textContent = e.target.textContent;
+        }
+        if (operation == "/") {
+          calculation.textContent = firstNumber / secondNumber;
+          output.textContent = e.target.textContent;
+        }
+        operation = "";
+        secondNumber = Number(calculation.textContent);
+        firstNumber = secondNumber;
+
+        console.log(
+          "Click nonFirst operation=> Num1: ",
+          firstNumber,
+          "Num2: ",
+          secondNumber,
+          "OP: ",
+          operation,
+          "click: ",
+          e.target.textContent,
+          "a: ",
+          output.textContent,
+          "b: ",
+          calculation.textContent
+        );
+      } else {
+        calculation.textContent += output.textContent + e.target.textContent;
+        firstNumber = secondNumber;
+
+        console.log(
+          "Click to First operation=> Num1: ",
+          firstNumber,
+          "Num2: ",
+          secondNumber,
+          "OP: ",
+          operation,
+          "click: ",
+          e.target.textContent,
+          "a: ",
+          output.textContent,
+          "b: ",
+          calculation.textContent
+        );
+      }
+
+      operation = e.target.textContent;
+      output.textContent = "";
+
+      console.log(
+        "After clicking operation=> Num1: ",
+        firstNumber,
+        "Num2: ",
+        secondNumber,
+        "OP: ",
+        operation,
+        "click: ",
+        e.target.textContent,
+        "a: ",
+        output.textContent,
+        "b: ",
+        calculation.textContent
+      );
+    }
+    //when user click on equal button
+    else if (e.target.className == "butons equal") {
+      // calculateIt(firstNumber, secondNumber, operation, output.textContent, calculation.textContent, e.target.textContent);
+      if (operation == "+") {
+        output.textContent = firstNumber + secondNumber;
+      }
+      if (operation == "-") {
+        output.textContent = firstNumber - secondNumber;
+      }
+      if (operation == "*") {
+        output.textContent = firstNumber * secondNumber;
+      }
+      if (operation == "/") {
+        output.textContent = firstNumber / secondNumber;
+      }
+      calculation.textContent = "";
+      operation = "";
+      secondNumber = Number(output.textContent);
+
+      console.log(
+        "Click on equal => Num1: ",
+        firstNumber,
+        "Num2: ",
+        secondNumber,
+        "OP: ",
+        operation,
+        "click: ",
+        e.target.textContent,
+        "a: ",
+        output.textContent,
+        "b: ",
+        calculation.textContent
+      );
+    }
+    //when user click on additional operators
+    else if (e.target.className == "butons additionalOperations") {
+      if (e.target.textContent == "AC") {
+        output.textContent = "";
+        calculation.textContent = "";
+
+        console.log(
+          "Click on AC=> Num1: ",
+          firstNumber,
+          "Num2: ",
+          secondNumber,
+          "OP: ",
+          operation,
+          "click: ",
+          e.target.textContent,
+          "a: ",
+          output.textContent,
+          "b: ",
+          calculation.textContent
+        );
+      } else if (e.target.textContent == "+-") {
+        console.log(
+          "Click on +- => Num1: ",
+          firstNumber,
+          "Num2: ",
+          secondNumber,
+          "OP: ",
+          operation,
+          "click: ",
+          e.target.textContent,
+          "a: ",
+          output.textContent,
+          "b: ",
+          calculation.textContent
+        );
+      } else if (e.target.textContent == "%") {
+        calculation.textContent += output.textContent + e.target.textContent;
+        output.textContent = Number(output.textContent) / 100;
+
+        console.log(
+          "Click on % => Num1: ",
+          firstNumber,
+          "Num2: ",
+          secondNumber,
+          "OP: ",
+          operation,
+          "click: ",
+          e.target.textContent,
+          "a: ",
+          output.textContent,
+          "b: ",
+          calculation.textContent
+        );
+      }
+
+      console.log(
+        "After additionalOperations=> Num1: ",
+        firstNumber,
+        "Num2: ",
+        secondNumber,
+        "OP: ",
+        operation,
+        "click: ",
+        e.target.textContent,
+        "a: ",
+        output.textContent,
+        "b: ",
+        calculation.textContent
+      );
+    }
+  }
 });
-
-
 
 // if (operation == "+") {
 //     calculation.textContent += output.textContent + e.target.textContent;
@@ -146,4 +273,3 @@ buttonArea.addEventListener("click", (e) => {
 //     calculation.textContent += output.textContent + e.target.textContent;
 //     output.textContent = firstNumber / secondNumber;
 //   }
-
